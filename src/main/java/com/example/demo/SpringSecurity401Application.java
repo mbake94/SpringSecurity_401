503e42cf70ad3fa84d392aebddd23961794dabe1
mbake94 <mbake94@gmail.com>
2020-06-02 08:54:57 -0700

    first commit

---
+++ src/main/java/com/example/demo/SpringSecurity401Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity401Application.class, args);
+    }
+
+}
